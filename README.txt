0517e0bbee318b055837f4401279e992bf28bb21
TimothyPLock <timothylock810@gmail.com>
2025-02-24 12:10:01 -0600

    adding icon files

---
+++ README.txt
@@ -0,0 +1,5 @@
+//Used deepseek to help with debugging and checking for syntax errors. As well as troubleshooting one error in css, helped me find !important.
+//The GithubCopilot extension was loaded into VSCode and was used with repeating lines of code with small variations.
+//used Web Dev Simplified's "How To Build A Weather App In JavaScript Without Needing A Server" video for help with css and the API.
+//Link to the video: https://youtu.be/w0VEOghdMpQ?si=vnW1D07IMH0WN8HB
+//Used favicon generator for the icon information including the header and icon files.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-//https://api.open-meteo.com/v1/forecast?latitude=36.165512&longitude=-85.48526&current=temperature_2m,relative_humidity_2m,weather_code&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago&forecast_days=1&models=icon_seamless
-
 async function getWeather() {
     //fetching weather data
     try{
@@ -85,4 +83,8 @@ function sendWeatherInfo(weatherData){
 
 getWeather(); //function call
 
-
+//Used deepseek to help with debugging and checking for syntax errors. As well as troubleshooting one error in css, helped me find !important.
+//The GithubCopilot extension was loaded into VSCode and was used with repeating lines of code with small variations.
+//used Web Dev Simplified's "How To Build A Weather App In JavaScript Without Needing A Server" video for help with css and the API.
+//Link to the video: https://youtu.be/w0VEOghdMpQ?si=vnW1D07IMH0WN8HB
+//Used favicon generator for the icon information including the header and icon files.
